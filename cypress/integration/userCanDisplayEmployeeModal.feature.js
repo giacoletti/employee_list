b0504199bcd61fe47ebe5877d12ba0a1605a2497
giacoletti <giacoletti92@gmail.com>
2021-11-22 17:41:01 +0100

    Adding end2end test to check if user is able to open up employee modal by clicking relative button

---
+++ cypress/integration/userCanDisplayEmployeeModal.feature.js
@@ -0,0 +1,20 @@
+describe('Visiting the application, a user can click the view button next to an employee', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('[data-cy=employee-1]').within(() => {
+      cy.get('.view-employee-button').click();
+    });
+  });
+
+  it('is expected to open up a modal', () => {
+    cy.get('[data-cy=modal-container]').should('be.visible');
+  });
+
+  it("is expected to see in the modal employee's name, image and email", () => {
+    cy.get('[data-cy=modal-container]').within(() => {
+      cy.get('[data-cy=employee-name]').should('contain.text', 'Thomas Bluth');
+      cy.get('[data-cy=employee-image]').should('be.visible');
+      cy.get('[data-cy=employee-email]').should('contain.text', 'george.bluth@reqres.in');
+    });
+  });
+});
