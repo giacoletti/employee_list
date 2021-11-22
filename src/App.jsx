95bc8973358b11629b9c6b6fffc4de88b72aa0ed
giacoletti <giacoletti92@gmail.com>
2021-11-22 17:44:28 +0100

    Creating components folder in src, moving EmployeeList.jsx in components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import EmployeeList from "./EmployeeList";
+import EmployeeList from "./components/EmployeeList";
 import { Container, Header } from "semantic-ui-react";
 
 const App = () => {
