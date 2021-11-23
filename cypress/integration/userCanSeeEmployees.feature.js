e9fad55991a9ccfa40f0be73f9ec152c05c4c304
giacoletti <giacoletti92@gmail.com>
2021-11-23 09:43:30 +0100

    Replacing uncaught exception method to catch only uncaught exceptions originated from unhandled promise rejectionsin userCanSeeEmployees.feature.js end2end test

--- cypress/integration/userCanSeeEmployees.feature.js
+++ cypress/integration/userCanSeeEmployees.feature.js
@@ -6,11 +6,16 @@ describe('Visiting the application, a user', () => {
     cy.visit('/');
   });
 
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
+  Cypress.on('uncaught:exception', (err, runnable, promise) => {
+    // when the exception originated from an unhandled promise
+    // rejection, the promise is provided as a third argument
+    // you can turn off failing the test in this case
+    if (promise) {
+      return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+  })
 
   it('is expected to see a header', () => {
     cy.get('[data-cy=employee-header]').should('contain.text', 'Employee List');
